565146680fc713aedafdb933391e765893854096
Laszlo Bodi <lazlo.bodi@protonmail.com>
2023-05-05 23:56:04 +0200

    add extra info for swagger

--- src/main/java/com/lazlob/cityviewer/controllers/CityController.java
+++ src/main/java/com/lazlob/cityviewer/controllers/CityController.java
@@ -4,7 +4,10 @@ import com.lazlob.cityviewer.models.dtos.CitiesPaginatedResponse;
 import com.lazlob.cityviewer.models.dtos.CityUpdateRequest;
 import com.lazlob.cityviewer.models.entities.City;
 import com.lazlob.cityviewer.services.CityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
@@ -21,19 +24,29 @@ public class CityController {
 
     CityService cityService;
 
+    @Operation(summary = "Get a city by id.")
+    @ApiResponse(responseCode = "200", description = "City retired successfully")
+    @ApiResponse(responseCode = "404", description = "City not found")
     @GetMapping("/{id}")
     public City getCityById(@PathVariable("id") Long id) {
         return cityService.getCityById(id);
     }
 
+    @Operation(summary = "Get all cities paginated, and optionally filtered by name.")
+    @ApiResponse(responseCode = "200", description = "Cities retrieved successfully")
+    @ApiResponse(responseCode = "400", description = "Page, size or nameSearch params are invalid")
     @GetMapping()
     public CitiesPaginatedResponse getCitiesPaginated(
             @RequestParam(value = "page", defaultValue = "0", required = false) @PositiveOrZero Integer page,
             @RequestParam(value = "size", defaultValue = "100", required = false) @Positive Integer size,
-            @RequestParam(value = "nameSearch", defaultValue ="", required = false) String nameSearch) {
+            @RequestParam(value = "nameSearch", defaultValue = "", required = false) @Pattern(regexp = "^([a-zA-Z\\s]{0,30})$") String nameSearch) {
         return cityService.getAllCitiesPaginated(page, size, nameSearch);
     }
 
+    @Operation(summary = "Update a city if exists.")
+    @ApiResponse(responseCode = "200", description = "City updated successfully")
+    @ApiResponse(responseCode = "400", description = "City update request body is invalid")
+    @ApiResponse(responseCode = "404", description = "City not found")
     @PutMapping("/{id}")
     public City updateCityById(
             @PathVariable("id") Long id,
