bd2637e47d868da2190c5fc879a0d7bb6f83504f
Laszlo Bodi <lazlo.bodi@protonmail.com>
2023-05-05 20:12:07 +0200

    inital

---
+++ src/main/java/com/lazlob/cityviewer/CityviewerApplication.java
@@ -0,0 +1,13 @@
+package com.lazlob.cityviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazlob/cityviewer/CityviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazlob.cityviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
