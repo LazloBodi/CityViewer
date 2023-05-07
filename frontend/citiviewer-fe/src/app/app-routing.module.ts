63d5110fd3b90812b96c67fe080319ac996b3885
Laszlo Bodi <lazlo.bodi@protonmail.com>
2023-05-07 21:58:46 +0200

    add route guard

--- frontend/citiviewer-fe/src/app/app-routing.module.ts
+++ frontend/citiviewer-fe/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CityListComponent } from './components/city-list/city-list.component';
 import { CityEditComponent } from './components/city-edit/city-edit.component';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './filter/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'cities', pathMatch: 'full' },
-  { path: 'cities', component: CityListComponent },
-  { path: 'city/:id', component: CityEditComponent },
+  { path: 'cities', component: CityListComponent, canActivate: [authGuard] },
+  { path: 'city/:id', component: CityEditComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'cities' },
 ];
