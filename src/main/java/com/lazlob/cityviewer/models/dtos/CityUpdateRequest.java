2a7da6be5088ef1bf0004a48342a6ad25112105c
Laszlo Bodi <lazlo.bodi@protonmail.com>
2023-05-14 21:12:16 +0200

    edit dto schema info

--- src/main/java/com/lazlob/cityviewer/models/dtos/CityUpdateRequest.java
+++ src/main/java/com/lazlob/cityviewer/models/dtos/CityUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.lazlob.cityviewer.models.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -12,7 +13,9 @@ import lombok.NoArgsConstructor;
 public class CityUpdateRequest {
     @NotBlank
     @Pattern(regexp = "^([0-9a-zA-Z\\s]{0,100})$")
+    @Schema(description = "Name of city", example = "Budapest")
     private String name;
     @NotBlank
+    @Schema(description = "Url for photo of the city", example = "url")
     private String photo;
 }
