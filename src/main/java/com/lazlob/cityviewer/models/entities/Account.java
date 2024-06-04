c2136e2c2d386de3715d7e4192f8705536c77786
Laszlo Bodi <lazlo.bodi@protonmail.com>
2024-06-04 22:28:01 +0200

    fix hibernate issues with @Data

--- src/main/java/com/lazlob/cityviewer/models/entities/Account.java
+++ src/main/java/com/lazlob/cityviewer/models/entities/Account.java
@@ -3,16 +3,36 @@ package com.lazlob.cityviewer.models.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+import java.util.Objects;
+
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Getter
+@Setter
 public class Account {
+
     @Id
     private String username;
+
     private String password;
     private String roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var other = (Account) o;
+        return Objects.equals(username, other.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
 }
--- src/main/java/com/lazlob/cityviewer/models/entities/City.java
+++ src/main/java/com/lazlob/cityviewer/models/entities/City.java
@@ -4,18 +4,35 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import java.util.Objects;
+
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Getter
+@Setter
 public class City {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private String name;
     private String photo;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var other = (City) o;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
